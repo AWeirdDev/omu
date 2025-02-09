c133b16dfb7932cb13dc01e2d8d7718e0a58d365
AWeirdScratcher <aweirdscratcher@gmail.com>
2025-02-09 19:10:02 +0800

    commit

--- omu-macros/examples/event.rs
+++
@@ -1,7 +0,0 @@
-use omu_macros::event;
-
-#[event(ready)]
-async fn on_ready() {}
-fn main() {
-    println!("{:?}", __omu_on_ready_metadata());
-}
--- omu-macros/examples/slash.rs
+++
@@ -1,8 +0,0 @@
-use omu_macros::slash;
-
-#[slash]
-async fn world() {}
-
-fn main() {
-    println!("{}", __omu_world_metadata());
-}
